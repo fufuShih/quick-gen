bdb7279f969efdc4c8e2b0e95712f0b99f140608
FelixShih <zx8714245@gmail.com>
2024-12-25 22:10:03 +0800

    refactor: remove deprecated files and configurations from react-doc package

--- packages/react-doc/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  testMatch: ['**/src/test/**/*.test.js'],
-  moduleFileExtensions: ['js', 'jsx']
-}; 
--- packages/react-doc/src/test/fixtures/components/FunctionButton.jsx
+++
@@ -1,9 +0,0 @@
-function FunctionButton({ type, onClick, children }) {
-  return (
-    <button type={type} onClick={onClick}>
-      {children}
-    </button>
-  );
-}
-
-export default FunctionButton; 
